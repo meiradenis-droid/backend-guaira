79516b3dbcb195961b8ed02421b2f3cec4ee1997
willyan <meira.denis@escola.pr.gov.br>
2026-03-11 15:21:20 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,9 @@
+console.log("Endereço Completo");
+
+let rua = "Rua professor rubens elke braga";
+let numero = 1199;
+let cidade = "Curitiba";
+
+let endereco = rua + ", " + numero + " - " + cidade;
+//console.log(endereco);
+console.log("O endereço correto é: " + endereco);
