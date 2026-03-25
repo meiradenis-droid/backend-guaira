4ecf3dd54bc66dcccb6ca3c1e9a3bade9c7da480
willyan <meira.denis@escola.pr.gov.br>
2026-03-25 18:48:22 +0000

    versao3

--- temperatura.js
+++ temperatura.js
@@ -1,6 +1,10 @@
 //conversor de temperatura
 const cidade = "Curitiba";
-let tempCelsius = 18.5;
+let tempCelsius = 26;
 
 const tempFahrenheit = tempCelsius * (9/5) + 32;
 const tempKelvin = tempCelsius + 273.15;
+
+console.log(`A temperatura de ${cidade} esta ${tempCelsius} Cº`);
+console.log(`Fahrenheit: ${tempFahrenheit.toFixed(1)} Fº`);
+console.log(`Kelvin: ${tempKelvin.toFixed(1)} Kº`);
