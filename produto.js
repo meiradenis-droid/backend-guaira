21a7f4f2a0449f67a90cad84feb9a6a58125c7b1
willyan <meira.denis@escola.pr.gov.br>
2026-03-25 18:27:55 +0000

    versao1

---
+++ produto.js
@@ -0,0 +1,9 @@
+const produto = "Mochila impermável";
+let preco = 189.90;
+let estoque = 14;
+const categoria = "acessório";
+
+console.log(`O produto é: ${produto}. `)
+console.log(`A ${produto} custa R$ ${preco.toFixed(2)}.`);
+console.log(`O estoque ainda possui ${estoque} mochilas.`);
+console.log(`A categoria de ${produto} é ${categoria}.`);
