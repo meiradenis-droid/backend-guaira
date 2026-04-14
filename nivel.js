ad2ab2da065163560118b7bdc241cbe1757f140d
willyan <meira.denis@escola.pr.gov.br>
2026-04-14 17:35:32 +0000

    v1

---
+++ nivel.js
@@ -0,0 +1,8 @@
+function subirDenivel(nivelAtual) {
+    return nivelAtual + 1;
+}
+
+let meuNivel = 10;
+let nivelAtualizado = subirDenivel(meuNivel); 
+
+console.log("Seu nível atual é: " + meuNivel)
