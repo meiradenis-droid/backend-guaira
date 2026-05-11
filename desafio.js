b45241a5858c0343df864b7c5268f05b7fdbc25c
willyan <meira.denis@escola.pr.gov.br>
2026-05-11 19:11:54 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,10 @@
+//🔵 Declare 5 variáveis de um perfil de um jogador
+
+let nomejogador = "Neymar"; 
+let posicao = "ponta esquerda";
+let idade = "34"
+let numeroCamisa = "11"
+let cuble = "Real Madrid"
+//console.log(numeroCamisa)
+console.log(`O jogador ${nomejogador}, joga como ${posicao}`);
+console.log(`O jogador ${nomejogador}, tem ${idade} anos`);
