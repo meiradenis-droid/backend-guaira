89d7a11411817a123b6149340fd22f30fdda1626
willyan <meira.denis@escola.pr.gov.br>
2026-05-20 16:39:51 +0000

    v1

--- desafio.js
+++ desafio.js
@@ -10,3 +10,10 @@ console.log(`O jogador ${nomejogador}, joga como ${posicao}`);
 console.log(`O jogador ${nomejogador}, tem ${idade} anos`);
 console.log(`o jogador ${nomejogador}, joga no ${cuble}`)
 
+let gols = "3"
+let carTao = "1"
+let subisTituido = "1"
+
+console.log(`o jogador ${nomejogador}, faz ${gols} gols a cada jogo`)
+console.log(`o jogador ${nomejogador}, fez uma falta e ganhou ${carTao} cartao!`)
+console.log(`o jogador ${nomejogador}, é subistituido ${subisTituido} vez por jogo `)
